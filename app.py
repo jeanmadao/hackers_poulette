21346277947c6a8579147ae9573ef806140044e9
Pindjouf <109728085+pindjouf@users.noreply.github.com>
2024-06-16 00:49:31 +0200

    simplify /support to one function

--- app.py
+++ app.py
@@ -10,24 +10,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/support", methods=["GET"])
-def support_get():
-    return render_template("support.html")
-
-@app.route("/support", methods=["POST"])
-def support_post():
-    first_name =  request.form.get("first_name")
-    last_name =  request.form.get("last_name")
-    gender =  request.form.get("gender")
-    subjects = 0
-    if request.form.get("repair"):
-        subjects += 4
-    if request.form.get("order"):
-        subjects += 2
-    if request.form.get("others") or subjects == 0:
-        subjects += 1
-    message = request.form.get("gender")
-    return render_template("support.html")
+@app.route("/support", methods=["GET", "POST"])
+def support():
+    if request.method == 'POST':
+        first_name =  request.form.get("first_name")
+        last_name =  request.form.get("last_name")
+        gender =  request.form.get("gender")
+        subjects = 0
+        if request.form.get("repair"):
+            subjects += 4
+        if request.form.get("order"):
+            subjects += 2
+        if request.form.get("others") or subjects == 0:
+            subjects += 1
+        message = request.form.get("gender")
+        return render_template("support.html")
+    else:
+        return render_template("support.html")
 
 if __name__ == "__main__":
     app.run()
