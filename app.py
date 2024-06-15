49da86bee32c98482d079ac85fae0571231db73b
Jean Huynh <huynhjeanpierre@hotmail.com>
2024-06-15 23:04:17 +0200

    cooking some css

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import (
     Flask,
-    render_template
+    render_template,
+    request
 )
 
 app = Flask(__name__)
@@ -9,8 +10,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/support")
-def support():
+@app.route("/support", methods=["GET"])
+def support_get():
+    return render_template("support.html")
+
+@app.route("/support", methods=["POST"])
+def support_post():
+    first_name =  request.form.get("first_name")
+    last_name =  request.form.get("last_name")
+    gender =  request.form.get("gender")
+    subjects = 0
+    if request.form.get("repair"):
+        subjects += 4
+    if request.form.get("order"):
+        subjects += 2
+    if request.form.get("others") or subjects == 0:
+        subjects += 1
+    message = request.form.get("gender")
     return render_template("support.html")
 
 if __name__ == "__main__":
