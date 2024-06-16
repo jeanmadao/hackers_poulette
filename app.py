654ea5f5deeb26127221934342d93f3d8228e342
Jean Huynh <huynhjeanpierre@hotmail.com>
2024-06-16 13:51:56 +0200

    input error display

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import (
     Flask,
+    redirect,
     render_template,
     request
 )
+
+from support_model import Support
 
 app = Flask(__name__)
 
@@ -12,12 +15,13 @@ def index():
 
 @app.get("/support")
 def support_get():
-    return render_template("support.html")
+    return render_template("support.html", support=Support())
 
 @app.post("/support")
 def support_post():
     first_name =  request.form.get("first_name")
     last_name =  request.form.get("last_name")
+    country =  request.form.get("country")
     gender =  request.form.get("gender")
     subjects = 0
     if request.form.get("repair"):
@@ -27,7 +31,13 @@ def support_post():
     if request.form.get("others") or subjects == 0:
         subjects += 1
     message = request.form.get("message")
-    return render_template("support.html")
+    support = Support(first_name, last_name, country, gender, subjects, message)
+    if support.validate():
+        return redirect("/")
+    else:
+        return render_template("support.html", support=support)
+
+
 
 if __name__ == "__main__":
     app.run()
--- support_model.py
+++ support_model.py
@@ -1,20 +1,24 @@
 class Support:
-    def __init__(self, first_name, last_name, gender, subjects, message):
+    def __init__(self, first_name=None, last_name=None, country=None, gender=None, subjects=0, message=None):
         self.first_name = first_name
         self.last_name = last_name
+        self.country = country
         self.gender = gender
         self.subjects = subjects
         self.message = message
         self.errors = {}
 
     def validate(self):
+        self.errors = {}
         if not self.first_name:
             self.errors["first_name"] = "First name required!"
         if not self.last_name:
             self.errors["last_name"] = "Last name required!"
+        if not self.country:
+            self.errors["country"] = "Country required!"
         if not self.gender:
             self.errors["gender"] = "Gender required!"
-        if not self.subjects:
-            self.errors["subjects"] = "Subjects required!"
         if not self.message:
             self.errors["message"] = "Message required!"
+
+        return False if self.errors else True
